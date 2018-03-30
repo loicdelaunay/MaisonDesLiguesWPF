0bfc91d910981ea04cc1252c2d293dd93a696f61
Loïc DELAUNAY <loicdelaunay05@gmail.com>
2018-03-30 13:36:27 +0200

    Add material ui

--- MaisonDesLiguesWpf/WpfPrincipale.xaml.cs
+++ MaisonDesLiguesWpf/WpfPrincipale.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace MaisonDesLiguesWpf
     /// <summary>
     /// Logique d'interaction pour WinPrincipale.xaml
     /// </summary>
-    public partial class WinPrincipale : Window
+    public partial class WinPrincipale : MetroWindow
     {
         public WinPrincipale()
         {
@@ -25,7 +26,6 @@ namespace MaisonDesLiguesWpf
             ViewComplementInscription.Visibility = Visibility.Hidden;
             ViewNuites.Visibility = Visibility.Hidden;
             ViewBenevole.Visibility = Visibility.Hidden;
-
         }
 
         /// <summary>
