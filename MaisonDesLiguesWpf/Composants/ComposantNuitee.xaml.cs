47a0128b11f33dbdc8e28221a7fa9c1aa457a4e9
Loïc DELAUNAY <loicdelaunay05@gmail.com>
2018-04-04 11:49:21 +0200

    Add : composant nuitee

---
+++ MaisonDesLiguesWpf/Composants/ComposantNuitee.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaisonDesLiguesWpf
+{
+    /// <summary>
+    /// Logique d'interaction pour ComposantNuitee.xaml
+    /// </summary>
+    public partial class ComposantNuitee
+    {
+
+        private Boolean isChecked;
+        private DateTime laDate;
+        private List<String> lesHotels;
+        private List<String> lesChambres;
+
+        public ComposantNuitee()
+        {
+            InitializeComponent();
+            Combobox_LesChambres.Items.Add("Test");
+            Combobox_LesChambres.Items.Add("Test2");
+            Combobox_LesChambres.Items.Add("Test3");
+            Combobox_LesHotels.Items.Add("Test");
+            Combobox_LesHotels.Items.Add("Test2");
+            Combobox_LesHotels.Items.Add("Test3");
+            this.SetLaDate(new DateTime(2011,05,11));
+        }
+
+        /// <summary>
+        /// Setter laDate
+        /// </summary>
+        /// <param name="uneDate"></param>
+        public void SetLaDate(DateTime uneDate)
+        {
+            this.laDate = uneDate;
+            Lbl_laDate.Content = this.laDate.ToString();
+        }
+
+        /// <summary>
+        /// Getter de la date
+        /// </summary>
+        /// <returns></returns>
+        public DateTime GetLaDate()
+        {
+            return this.laDate;
+        }
+
+        public Boolean IsChecked()
+        {
+            return (Boolean)Chbx_laCheckbox.IsChecked;
+        }
+
+        /// <summary>
+        /// Ajoute un hotel
+        /// </summary>
+        /// <param name="unHotel"></param>
+        public void AddHotel(string unHotel)
+        {
+            lesHotels.Add(unHotel);
+            Combobox_LesHotels.Items.Add(unHotel);
+        }
+
+        /// <summary>
+        /// Ajoute un hotel
+        /// </summary>
+        /// <param name="uneChambre"></param>
+        public void AddChambre(string uneChambre)
+        {
+            lesChambres.Add(uneChambre);
+            Combobox_LesChambres.Items.Add(uneChambre);
+        }
+
+        /// <summary>
+        /// Quand l'utilisateur cliquer sur la case à cocher
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CheckedClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- MaisonDesLiguesWpf/WpfPrincipale.xaml.cs
+++ MaisonDesLiguesWpf/WpfPrincipale.xaml.cs
@@ -22,14 +22,20 @@ namespace MaisonDesLiguesWpf
     /// </summary>
     public partial class WinPrincipale : MetroWindow
     {
+        internal BaseDeDonnees.Bdd UneConnexion;
+
         public WinPrincipale()
         {
             InitializeComponent();
             ViewComplementInscription.Visibility = Visibility.Hidden;
             ViewNuites.Visibility = Visibility.Hidden;
             ViewBenevole.Visibility = Visibility.Hidden;
+
+            new ComposantNuitee
+            {
+                Margin = new Thickness(400, 400, 200, 200)
+            };
         }
-        internal BaseDeDonnees.Bdd UneConnexion;
 
         public void InitBddConnexion(Bdd UneConnexionOracle)
         {
@@ -99,7 +105,7 @@ namespace MaisonDesLiguesWpf
         public void GererInscriptionBenevole()
         {
             ViewBenevole.Visibility = Visibility.Visible;
-            ViewBenevole.Margin = new Thickness(28,382,467,239);
+            ViewBenevole.Margin = new Thickness(40, 433, 599, 103);
             ViewComplementInscription.Visibility = Visibility.Hidden;
         }
 
@@ -111,17 +117,17 @@ namespace MaisonDesLiguesWpf
 
         private void GererInscriptionIntervenant()
         {
-            ViewComplementInscription.Visibility = Visibility.Visible;
-            ViewBenevole.Visibility = Visibility.Hidden;
-            ViewNuites.Visibility = Visibility.Visible;
-            //ViewLicencie.Visibility = Visibility.Hidden;
-            PanFonctionIntervenant.Visibility = Visibility.Visible;
-            Thickness margin = new Thickness();
-            margin.Top = 264;
-            margin.Left = 23;
-            GrpIntervenant.Margin = margin;
-            Utilitaire.CreerDesControles(this, UneConnexion, "VSTATUT01", "Rad_", PanFonctionIntervenant, "RadioButton", this.rdbStatutIntervenant_StateChanged);
-            Utilitaire.RemplirComboBox(UneConnexion, ComboboxComplementInscription, "VATELIER01");
+            //ViewComplementInscription.Visibility = Visibility.Visible;
+            //ViewBenevole.Visibility = Visibility.Hidden;
+            //ViewNuites.Visibility = Visibility.Visible;
+            ////ViewLicencie.Visibility = Visibility.Hidden;
+            //PanFonctionIntervenant.Visibility = Visibility.Visible;
+            //Thickness margin = new Thickness();
+            //margin.Top = 264;
+            //margin.Left = 23;
+            //GrpIntervenant.Margin = margin;
+            //Utilitaire.CreerDesControles(this, UneConnexion, "VSTATUT01", "Rad_", PanFonctionIntervenant, "RadioButton", this.rdbStatutIntervenant_StateChanged);
+            //Utilitaire.RemplirComboBox(UneConnexion, ComboboxComplementInscription, "VATELIER01");
 
             ComboboxComplementInscription.DataContext = "Choisir";
         }
