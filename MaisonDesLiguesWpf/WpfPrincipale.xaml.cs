5eb99f9aca201fcfbca7dc9494360e61802ad15f
Guillaume Cauvet <cauvetg@gmail.com>
2018-05-10 14:57:32 +0200

    Fix : Bouton enregistrer intervenant/benevole

--- MaisonDesLiguesWpf/WpfPrincipale.xaml.cs
+++ MaisonDesLiguesWpf/WpfPrincipale.xaml.cs
@@ -229,5 +229,90 @@ namespace MaisonDesLiguesWpf
             }
         }
 
+        /// <summary>
+        /// <summary> 
+        /// Permet d'intercepter le click sur le bouton d'enregistrement d'un bénévole. 
+        /// Cetteméthode va appeler la méthode InscrireBenevole de la Bdd, après avoir mis en forme certains paramètres à envoyer. 
+        /// </summary> 
+        /// <param name="sender"></param> 
+        /// <param name="e"></param> 
+        private void BtnEnregistreBenevole_Click(object sender, RoutedEventArgs e)
+        {
+            Collection<Int16> IdDatesSelectionnees = new Collection<Int16>();
+            Int64? NumeroLicence;
+            if (TxtLicenceBenevole.IsMaskFull)
+            {
+                NumeroLicence = System.Convert.ToInt64(TxtLicenceBenevole.Text);
+            }
+            else
+            {
+                NumeroLicence = null;
+            }
+
+            foreach (Control UnControle in PanelDispoBenevole.Children)
+            {
+                if (UnControle.GetType().Name == "CheckBox" && ((CheckBox)UnControle).IsChecked == true)
+                {
+                    /* Un name de controle est toujours formé come ceci : xxx_Id où id représente l'id dans la table
+                     * Donc on splite la chaine et on récupére le deuxième élément qui correspond à l'id de l'élément sélectionné.
+                     * on rajoute cet id dans la collection des id des dates sélectionnées
+                        
+                    */
+                    IdDatesSelectionnees.Add(System.Convert.ToInt16((UnControle.Name.Split('_'))[1]));
+                }
+            }
+            UneConnexion.InscrireBenevole(TxtboxNom.Text, TextboxPrenom.Text, TxtboxAdresse1.Text, TxtboxAdresse2.Text != "" ? TxtboxAdresse2.Text : null, Msktextbox_CP.Text, TextboxVille.Text, Msktextbox_Tel.IsMaskFull ? Msktextbox_Tel.Text : null, TextboxMail.Text != "" ? TextboxMail.Text : null, System.Convert.ToDateTime(TxtDateNaissance.Text), NumeroLicence, IdDatesSelectionnees);
+
+        }
+
+        /// <summary> 
+        /// Cette procédure va appeler la procédure .... qui aura pour but d'enregistrer les éléments  
+        /// de l'inscription d'un intervenant, avec éventuellment les nuités à prendre en compte        /// 
+        /// </summary> 
+        /// <param name="sender"></param> 
+        /// <param name="e"></param> 
+        private void BtnEnregistrerIntervenant_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (RadNuiteOui.IsChecked == true)
+                {
+                    // inscription avec les nuitées 
+                    Collection<Int16> NuitsSelectionnes = new Collection<Int16>();
+                    Collection<String> HotelsSelectionnes = new Collection<String>();
+                    Collection<String> CategoriesSelectionnees = new Collection<string>();
+                    //TODO foreach (Control UnControle in PanNuiteIntervenant.Children)
+                    //{
+                    //    if (UnControle.GetType().Name == "ResaNuite" && ((ComposantNuitee)UnControle).GetNuitSelectionnee())
+                    //    {
+                    //        // la nuité a été cochée, il faut donc envoyer l'hotel et la type de chambre à la procédure de la base qui va enregistrer le contenu hébergement  
+                    //        //ContenuUnHebergement UnContenuUnHebergement= new ContenuUnHebergement(); 
+                    //        CategoriesSelectionnees.Add(((ComposantNuitee)UnControle).GetChambreSelected());
+                    //        HotelsSelectionnes.Add(((ComposantNuitee)UnControle).GetHotelSelected());
+                    //        NuitsSelectionnes.Add(((ComposantNuitee)UnControle).IdNuite);
+                    //    }
+                    //}
+                    if (NuitsSelectionnes.Count == 0)
+                    {
+                        MessageBox.Show("Si vous avez sélectionné que l'intervenant avait des nuités\n in faut qu'au moins une nuit soit sélectionnée");
+                    }
+                    else
+                    {
+                        UneConnexion.InscrireIntervenant(TxtboxNom.Text, TextboxPrenom.Text, TxtboxAdresse1.Text, TxtboxAdresse2.Text != "" ? TxtboxAdresse2.Text : null, Msktextbox_CP.Text, TextboxVille.Text, Msktextbox_Tel.IsMaskFull ? Msktextbox_Tel.Text : null, TextboxMail.Text != "" ? TextboxMail.Text : null, System.Convert.ToInt16(ComboboxComplementInscription.SelectedValue), this.IdStatutSelectionne, CategoriesSelectionnees, HotelsSelectionnes, NuitsSelectionnes);
+                        MessageBox.Show("Inscription intervenant effectuée");
+                    }
+                }
+                else
+                { // inscription sans les nuitées 
+                    UneConnexion.InscrireIntervenant(TxtboxNom.Text, TextboxPrenom.Text, TxtboxAdresse1.Text, TxtboxAdresse2.Text != "" ? TxtboxAdresse2.Text : null, Msktextbox_CP.Text, TextboxVille.Text, Msktextbox_Tel.IsMaskFull ? Msktextbox_Tel.Text : null, TextboxMail.Text != "" ? TextboxMail.Text : null, System.Convert.ToInt16(ComboboxComplementInscription.SelectedValue), this.IdStatutSelectionne);
+                    MessageBox.Show("Inscription intervenant effectuée");
+
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
     }
 }
